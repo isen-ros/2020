8a8bc2a27f465118ca04f12fbc16339eb7cc14a1
Beatrice-AB <57352665+Beatrice-AB@users.noreply.github.com>
2020-01-22 17:13:54 +0100

    Update akongelabeatrice.sh

--- akongela/class_2/akongelabeatrice.sh
+++ akongela/class_2/akongelabeatrice.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 xterm -e roslaunch test_prog_c turtlemimic.launch &
-xterm -e rosrun test_prog_c drawingsquare
+
 #xterm -e rosrun test_prog_c drawingsquare
+
+xterm -e rosrun test_prog_c drawingsquare
+
