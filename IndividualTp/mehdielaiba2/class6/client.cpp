02b26a915c76638ecff5cb6b6bcc6f9f3c33888f
mehdi <mehdi.elaiba@isen.yncrea.fr>
2020-02-21 06:19:32 -0800

    class 6

---
+++ IndividualTp/mehdielaiba2/class6/client.cpp
@@ -0,0 +1,41 @@
+#include "ros/ros.h"
+#include "exercice6/serveur.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "client");
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<exercice6::serveur>("serveur");
+  exercice6::serveur srv;
+  srv.request.a = 0;
+
+
+
+
+
+
+
+do{
+
+  if (client.call(srv))
+
+  {
+    ROS_INFO("Heure => [%ld] : [%ld] : [%ld]", 
+(long int)srv.response.horaire,srv.response.horaire1, srv.response.horaire2);
+  }
+
+
+  else
+  {
+    ROS_ERROR("Erreur");
+    return 1;
+  }
+
+ 
+  ros::Duration(1).sleep();
+
+}while(ros::ok());
+
+  return 0;
+}
---
+++ IndividualTp/mehdielaiba2/class6/heure.cpp
@@ -0,0 +1,31 @@
+#include "ros/ros.h"
+#include "exercice6/serveur.h"
+#include "stdio.h"
+#include "time.h"
+
+bool add(exercice6::serveur::Request &req, exercice6::serveur::Response &res)
+{
+  time_t secondes;
+  struct tm instant;
+
+  time(&secondes);
+  instant=*localtime(&secondes);
+
+  res.horaire = instant.tm_hour;
+  res.horaire1 = instant.tm_min;
+  res.horaire2 = instant.tm_sec;
+
+  ROS_INFO("Infomartion envoyée");
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "heure");
+  ros::NodeHandle n;
+  time_t secondes;
+  ros::ServiceServer service = n.advertiseService("serveur", add);
+  ROS_INFO("Correct");
+  ros::spin();
+  return 0;
+}
