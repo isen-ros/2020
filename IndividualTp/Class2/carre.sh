7f6e1028fa671fe6c24af940c80d4d9cb5fae461
MoloAlex <59566088+MoloAlex@users.noreply.github.com>
2020-03-08 23:03:47 +0100

    Delete carre.sh

--- IndividualTp/Class2/carre.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-rosservice call turtle1/teleport_relative 2 1.57
-rosservice call turtle1/teleport_relative 2 1.57
-rosservice call turtle1/teleport_relative 2 1.57
-rosservice call turtle1/teleport_relative 2 1.57
